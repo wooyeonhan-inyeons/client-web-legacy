941365dc4e21a3ce23b201911916ca00fd02bfb2
lee7198 <lee7198@naver.com>
2022-11-15 14:24:21 +0900

    feat: #18 antd 에러 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
