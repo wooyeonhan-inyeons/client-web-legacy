c064f58979b9c5a166a545f3042a2a818f8e1fca
lee7198 <lee7198@naver.com>
2022-11-15 15:44:15 +0900

    feat: #18 전역 변수 수정

--- src/color.ts
+++
@@ -1,7 +0,0 @@
-export const COLOR = {
-  primary: "#EDF1F7",
-  secondary: "#E3B719",
-  content: "#FFF",
-  subContent: "#FFFFFF70",
-  background: "#263145",
-};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,3 +9,15 @@ export enum SNS {
   GOOGLE,
   NONE,
 }
+export const COLOR = {
+  primary: "#EDF1F7",
+  secondary: "#E3B719",
+  content: "#FFF",
+  subContent: "#FFFFFF70",
+  background: "#263145",
+};
+
+export const zIndex = {
+  default: 999,
+  background: 0,
+};
