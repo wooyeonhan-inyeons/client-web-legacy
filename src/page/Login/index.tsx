b27c22584bbeafcd165e7671774d539877f09519
lee7198 <lee7198@naver.com>
2022-11-25 22:48:33 +0900

    feat: #28 로그인 UI 누락본 업데이트

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { SNS } from "../../constants";
 
-import { StyledContainer } from "../../components/StyledContainer";
 import { SnsButton } from "./Button";
 import { LoginTitle } from "./Title";
 import { Rectangle } from "./Rectangle";
@@ -12,33 +11,26 @@ import kakaoLogo from "./Button/img/kakaoLogo.png";
 
 const Login = () => {
   return (
-    <>
-      <StyledContainer>
-        <CenterBox>
-          <Rectangle />
-          <LoginTitle>
-            우연한 <br />
-            발견
-          </LoginTitle>
-          <div>
-            <SnsButton sns={SNS.KAKAO}>
-              <img src={kakaoLogo} />
-              <div className="title">Kakao 계정으로 로그인</div>
-            </SnsButton>
-            <SnsButton sns={SNS.GOOGLE}>
-              <img src={googleLogo} />
-              <div className="title">Google 계정으로 로그인</div>
-            </SnsButton>
-            <SnsButton
-              sns={SNS.NONE}
-              onClick={() => (window.location.href = "/")}
-            >
-              게스트로 볼래요
-            </SnsButton>
-          </div>
-        </CenterBox>
-      </StyledContainer>
-    </>
+    <CenterBox>
+      <Rectangle />
+      <LoginTitle>
+        우연한 <br />
+        발견
+      </LoginTitle>
+      <div>
+        <SnsButton sns={SNS.KAKAO}>
+          <img src={kakaoLogo} alt="kakao" />
+          <div className="title">Kakao 계정으로 로그인</div>
+        </SnsButton>
+        <SnsButton sns={SNS.GOOGLE}>
+          <img src={googleLogo} alt="google" />
+          <div className="title">Google 계정으로 로그인</div>
+        </SnsButton>
+        <SnsButton sns={SNS.NONE} onClick={() => (window.location.href = "/")}>
+          게스트로 볼래요
+        </SnsButton>
+      </div>
+    </CenterBox>
   );
 };
 
