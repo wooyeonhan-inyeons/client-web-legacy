22b8e57ff52fef6eac046e7b67b8bfb7640406f3
lee7198 <lee7198@naver.com>
2022-12-09 21:32:25 +0900

    feat: #35 MarkerCluster 적용

--- src/page/Home/Map/index.tsx
+++ src/page/Home/Map/index.tsx
@@ -183,81 +183,200 @@
 // };
 
 //3번 쨰 방법 ㅋㅋ
-import { useState, useMemo, useRef, useEffect } from "react";
-import {
-  GoogleMap,
-  Marker,
-  MarkerClusterer,
-  MarkerClustererF,
-  MarkerF,
-} from "@react-google-maps/api";
-import { useNavigate } from "react-router-dom";
-import useGeolocation from "react-hook-geolocation";
+// import { useState, useMemo, useRef, useEffect } from "react";
+// import {
+//   GoogleMap,
+//   Marker,
+//   MarkerClusterer,
+//   MarkerClustererF,
+//   MarkerF,
+// } from "@react-google-maps/api";
+// import { useNavigate } from "react-router-dom";
+// import useGeolocation from "react-hook-geolocation";
 
+// import { useRecoilState } from "recoil";
+// import { recoil_ } from "../../../recoil";
+// import { GetNearPost, getNearTest } from "./api/getPost";
+// import { useQuery } from "react-query";
+// import { MarkerImages } from "./components/Mark";
+// // import {markImage} from "./images/marker/defaultMark/"
+
+// type MapOptions = google.maps.MapOptions;
+
+// interface MarkerProps {
+//   content: string;
+//   created_time: string;
+//   forFriend: number;
+//   latitude: number;
+//   longitude: number;
+//   post_id: string;
+// }
+
+// export default function Map() {
+//   const navigate = useNavigate();
+//   const geolocation = useGeolocation();
+//   // const mapRef = useRef<GoogleMap>();
+//   // const [center, setCenter] = useState<LatLngLiteral>();
+//   const [data, setData] = useState([]);
+//   const [coordinate, setCoordinate] = useRecoilState<google.maps.LatLngLiteral>(
+//     recoil_.geoState
+//   );
+
+//   const options = useMemo<MapOptions>(
+//     () => ({
+//       mapId: "7c08bc77e896521d",
+//       backgroundColor: "#242f3e",
+//       minZoom: 13,
+//       // maxZoom: 20,
+//       zoomControl: false,
+//       mapTypeControl: false,
+//       scaleControl: false,
+//       streetViewControl: false,
+//       rotateControl: false,
+//       fullscreenControl: false,
+//       clickableIcons: false,
+//     }),
+//     []
+//   );
+//   // const onLoad = useCallback((map: any) => (mapRef.current = map));
+
+//   // const { data, isLoading, refetch } = useQuery(
+//   //   "postNear",
+//   //   () => GetNearPost(coordinate),
+//   //   {
+//   //     refetchOnMount: "always",
+//   //     retryDelay: 0,
+//   //     refetchInterval: 0,
+//   //     retry: 2,
+//   //     staleTime: 0,
+//   //     cacheTime: Infinity,
+//   //     onError: () => {
+//   //       console.log("ERROR");
+//   //     },
+//   //     onSuccess: () => {
+//   //       console.log("useQuery: ", data);
+//   //     },
+//   //   }
+//   // );
+
+//   useEffect(() => {
+//     if (geolocation.latitude !== null) {
+//       setCoordinate({
+//         lat: geolocation.latitude,
+//         lng: geolocation.longitude,
+//       });
+//     }
+//   }, [geolocation]);
+
+//   useEffect(() => {
+//     getNearTest(coordinate)?.then((res) => {
+//       setData(res);
+//     });
+//   }, [coordinate]);
+
+//   return (
+//     <GoogleMap
+//       zoom={17}
+//       center={coordinate}
+//       mapContainerClassName="map-container"
+//       options={options}
+//       // onLoad={onLoad}
+//     >
+//       {/* <MarkerClustererF>
+//         {(clusterer) => ( */}
+//       <>
+//         {data?.map((item: MarkerProps, index: number) => (
+//           <Marker
+//             key={item.post_id}
+//             onClick={() => navigate(`/detail/${item.post_id}`)}
+//             position={{ lat: item.latitude, lng: item.longitude }}
+//             icon={
+//               item.forFriend
+//                 ? MarkerImages.friend[index % 7]
+//                 : MarkerImages.default[index % 7]
+//             }
+//             cursor="pointer"
+//             // clusterer={clusterer}
+//           />
+//         ))}
+//       </>
+//       {/* )}
+//       </MarkerClustererF> */}
+//     </GoogleMap>
+//   );
+// }
+
+//4번째 방법
+import react, { useEffect, useRef, useState } from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import {
+  Cluster,
+  DefaultRenderer,
+  MarkerClusterer,
+  Renderer,
+} from "@googlemaps/markerclusterer";
+import React from "react";
+import useGeolocation from "react-hook-geolocation";
 import { useRecoilState } from "recoil";
 import { recoil_ } from "../../../recoil";
-import { GetNearPost, getNearTest } from "./api/getPost";
-import { useQuery } from "react-query";
+import { getNearTest } from "./api/getPost";
 import { MarkerImages } from "./components/Mark";
-// import {markImage} from "./images/marker/defaultMark/"
+import { useNavigate } from "react-router-dom";
 
-type MapOptions = google.maps.MapOptions;
+import MarkerClusterIcon from "./images/MarkerCluster.png";
+
+// const render = (status: Status): ReactElement => {
+//   if (status === Status.FAILURE) return <ErrorComponent />;
+//   return <Spinner />;
+// };
 
 interface MarkerProps {
   content: string;
   created_time: string;
-  forFriend: number;
   latitude: number;
   longitude: number;
   post_id: string;
+  forFriend: boolean;
+  viewed: boolean;
 }
 
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+
+const options: any = {
+  zoom: 17,
+  mapId: "7c08bc77e896521d",
+  backgroundColor: "#242f3e",
+  minZoom: 13,
+  // maxZoom: 20,
+  zoomControl: false,
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  rotateControl: false,
+  fullscreenControl: false,
+  clickableIcons: false,
+};
+
 export default function Map() {
+  return (
+    <Wrapper apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY!}>
+      <MapComponent />
+    </Wrapper>
+  );
+}
+
+function MapComponent() {
   const navigate = useNavigate();
   const geolocation = useGeolocation();
-  // const mapRef = useRef<GoogleMap>();
-  // const [center, setCenter] = useState<LatLngLiteral>();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<MarkerProps[]>([]);
   const [coordinate, setCoordinate] = useRecoilState<google.maps.LatLngLiteral>(
     recoil_.geoState
   );
-
-  const options = useMemo<MapOptions>(
-    () => ({
-      mapId: "7c08bc77e896521d",
-      backgroundColor: "#242f3e",
-      minZoom: 13,
-      // maxZoom: 20,
-      zoomControl: false,
-      mapTypeControl: false,
-      scaleControl: false,
-      streetViewControl: false,
-      rotateControl: false,
-      fullscreenControl: false,
-      clickableIcons: false,
-    }),
-    []
-  );
-  // const onLoad = useCallback((map: any) => (mapRef.current = map));
-
-  // const { data, isLoading, refetch } = useQuery(
-  //   "postNear",
-  //   () => GetNearPost(coordinate),
-  //   {
-  //     refetchOnMount: "always",
-  //     retryDelay: 0,
-  //     refetchInterval: 0,
-  //     retry: 2,
-  //     staleTime: 0,
-  //     cacheTime: Infinity,
-  //     onError: () => {
-  //       console.log("ERROR");
-  //     },
-  //     onSuccess: () => {
-  //       console.log("useQuery: ", data);
-  //     },
-  //   }
-  // );
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (geolocation.latitude !== null) {
@@ -274,34 +393,66 @@ export default function Map() {
     });
   }, [coordinate]);
 
-  return (
-    <GoogleMap
-      zoom={17}
-      center={coordinate}
-      mapContainerClassName="map-container"
-      options={options}
-      // onLoad={onLoad}
-    >
-      {/* <MarkerClustererF>
-        {(clusterer) => ( */}
-      <>
-        {data?.map((item: MarkerProps, index: number) => (
-          <Marker
-            key={item.post_id}
-            onClick={() => navigate(`/detail/${item.post_id}`)}
-            position={{ lat: item.latitude, lng: item.longitude }}
-            icon={
-              item.forFriend
-                ? MarkerImages.friend[index % 7]
-                : MarkerImages.default[index % 7]
-            }
-            cursor="pointer"
-            // clusterer={clusterer}
-          />
-        ))}
-      </>
-      {/* )}
-      </MarkerClustererF> */}
-    </GoogleMap>
+  useEffect(() => {
+    initMap(coordinate, data, navigate);
+    // window.initMap = initMap;
+  }, [data, initMap]);
+
+  return <div ref={ref!} id="map" />;
+}
+
+function initMap(
+  coordinate: google.maps.LatLngLiteral,
+  data: Array<MarkerProps>,
+  navigate: any
+): void {
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    { center: coordinate, ...options }
   );
+
+  console.log(data);
+  const markers = data.map((item: MarkerProps, index: number) => {
+    const marker = new google.maps.Marker({
+      position: { lat: item.latitude, lng: item.longitude },
+      icon: item.viewed
+        ? MarkerImages.viewed[index % 7]
+        : item.forFriend
+        ? MarkerImages.friend[index % 7]
+        : MarkerImages.default[index % 7],
+    });
+    marker.addListener("click", () => {
+      navigate(`detail/${item.post_id}`);
+    });
+    return marker;
+  });
+
+  var mcOptions = {
+    //imagePath: 'https://googlemaps.github.io/js-marker-clusterer/images/m',
+    styles: [
+      {
+        url: "https://googlemaps.github.io/js-marker-clusterer/images/m1.png",
+        width: 53,
+        height: 53,
+        fontFamily: "comic sans ms",
+        textSize: 15,
+        textColor: "red",
+        //color: #00FF00,
+      },
+    ],
+  };
+  new MarkerClusterer({
+    map,
+    markers,
+    renderer: {
+      render: ({ count, position }: Cluster) =>
+        new google.maps.Marker({
+          icon: MarkerClusterIcon,
+          label: { text: String(count), color: "white", fontSize: "10px" },
+          position,
+          // adjust zIndex to be above other markers
+          zIndex: Number(google.maps.Marker.MAX_ZINDEX) + count,
+        }),
+    },
+  });
 }
